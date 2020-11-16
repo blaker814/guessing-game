3b3778d45fdafd039bf31bcce8b21b5cf0f9c7b7
blaker814 <rab0196@yahoo.com>
2020-11-16 10:14:29 -0600

    phase 7 complete

--- Program.cs
+++ Program.cs
@@ -6,16 +6,37 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            int count = 1;
+            Console.WriteLine(@"Please choose your mode:
+            1) Easy Mode - 8 guesses
+            2) Medium Mode - 6 guesses
+            3) Hard Mode - 4 guesses");
+            int modeChoice = int.Parse(Console.ReadLine());
+            int numGuesses = 0;
+            if (modeChoice == 1)
+            {
+                Console.WriteLine("Easy mode activated");
+                numGuesses = 8;
+            }
+            else if (modeChoice == 2)
+            {
+                Console.WriteLine("Medium mode activated");
+                numGuesses = 6;
+            }
+            else if (modeChoice == 3)
+            {
+                Console.WriteLine("Hard mode activated");
+                numGuesses = 4;
+            }
+            int count = 0;
             Random r = new Random();
             int secretNum = r.Next(1, 101);
             int guess = 0;
 
-            while (count < 5 && guess != secretNum)
+            while (count < numGuesses && guess != secretNum)
             {
                 Console.WriteLine("Can you guess the secret number?");
-                Console.WriteLine($"Number of guesses left: {5 - count}");
-                if (count > 1)
+                Console.WriteLine($"Number of guesses left: {numGuesses - count}");
+                if (count > 0)
                 {
                     Console.Write($"Your guess ({guess}): ");
                 }
