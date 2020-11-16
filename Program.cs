1b10327eece3876fbe6ba43c2eec79c38123415b
blaker814 <rab0196@yahoo.com>
2020-11-16 09:40:00 -0600

    Phase 1 complete

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Can you guess the secret number?");
+            Console.Write("Your guess: ");
+            string guess = Console.ReadLine();
+            Console.WriteLine(guess);
+        }
+    }
+}
