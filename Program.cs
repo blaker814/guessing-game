fa58e81805ed1710cadc12ca9c12842b6e312f71
blaker814 <rab0196@yahoo.com>
2020-11-16 09:57:15 -0600

    Phase 4 complete

--- Program.cs
+++ Program.cs
@@ -8,12 +8,20 @@ namespace GuessingGame
         {
             int count = 1;
             int secretNum = 42;
+            int guess = 0;
 
-            while (count < 5)
+            while (count < 5 && guess != secretNum)
             {
                 Console.WriteLine("Can you guess the secret number?");
-                Console.Write("Your guess: ");
-                int guess = int.Parse(Console.ReadLine());
+                if (count > 1)
+                {
+                    Console.Write($"Your guess ({guess}): ");
+                }
+                else
+                {
+                    Console.Write($"Your guess: ");
+                }
+                guess = int.Parse(Console.ReadLine());
 
                 if (secretNum == guess)
                 {
